ae51fdf8e302e3beee6c2066c27ec5231fd9cda1
dewashish17 <91001502+dewashish17@users.noreply.github.com>
2023-01-14 12:47:56 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,21 @@
-class A{
+
+public class Main {
+  
+    class A{
   public void meth()
   {
     System.out.println("Invoking method from class A");
   }
 }
 class B extends A{
+  @Override
   public void meth()
   {
     System.out.println("Method is overridden in Extendend class B");
   }
-}
-public class Main {
-    
-    public static void main(String [] args)
+}   
+  
+     public static void main(String [] args)
     {
         A obj=new B();
         obj.meth();
