b190b83ee661568cfa646912376e409ba2f768e8
dewashish17 <91001502+dewashish17@users.noreply.github.com>
2023-01-14 12:53:20 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,9 +14,10 @@ public class Main {
       }
   }
   public static void main(String[] args){
-     A a = new B();
+     A a = new A();
      a.meth();
 
-    
+     B b = new B();
+     b.meth();
   }
 }
